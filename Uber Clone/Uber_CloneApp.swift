d02afe9b360ee645470240ec3d06db4029abd2f4
lowjungxuan <lowjungxuan@gmail.com>
2022-11-14 23:16:32 +0800

    Initial Commit

---
+++ Uber Clone/Uber_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Uber_CloneApp.swift
+//  Uber Clone
+//
+//  Created by Low Jung Xuan on 14/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Uber_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
