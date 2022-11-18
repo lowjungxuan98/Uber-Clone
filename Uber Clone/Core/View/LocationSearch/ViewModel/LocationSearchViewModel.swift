af32157df32e718e16799b4c99c1f1088f23a2a4
lowjungxuan <lowjungxuan@gmail.com>
2022-11-18 10:57:57 +0800

    Starting Module 6 Adding Annotation to MapView

--- Uber Clone/Core/View/LocationSearch/ViewModel/LocationSearchViewModel.swift
+++ Uber Clone/Core/View/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -12,13 +12,15 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     // MARK: = Properties
 
     @Published var results = [MKLocalSearchCompletion]()
-    @Published var selectedLocation: String?
+    @Published var selectedLocationCoordinate: CLLocationCoordinate2D?
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
         didSet {
             searchCompleter.queryFragment = queryFragment
         }
     }
+
+    // MARK: - Lifecycle
 
     override init() {
         super.init()
@@ -28,9 +30,25 @@ class LocationSearchViewModel: NSObject, ObservableObject {
 
     // MARK: - Helpers
 
-    func selectLocation(_ location: String) {
-        selectedLocation = location
-        print("DEBUG: Selected location is \(self.selectedLocation)")
+    func selectLocation(_ localSearch: MKLocalSearchCompletion) {
+        locationSearch(forLocalSearchCompletion: localSearch) { response, error in
+            if let error = error {
+                print("DEBUG : Location search failed with error \(error.localizedDescription)")
+                return
+            }
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+            self.selectedLocationCoordinate = coordinate
+            print("DEBUG : Location corrdinate \(coordinate)")
+        }
+    }
+
+    func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion, completion: @escaping MKLocalSearch.CompletionHandler) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+
+        search.start(completionHandler: completion)
     }
 }
 
