5e16ed384ed6b2af7aaad77527b75a14f2ffc097
lowjungxuan <lowjungxuan@gmail.com>
2022-11-18 15:09:39 +0800

    Starting Module 12 Pricing Model

---
+++ Uber Clone/Models/RideType.swift
@@ -0,0 +1,37 @@
+//
+//  RideType.swift
+//  Uber Clone
+//
+//  Created by Low Jung Xuan on 18/11/2022.
+//
+
+import Foundation
+enum RideType: Int, CaseIterable, Identifiable {
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue }
+    
+    var description: String {
+        switch self {
+        case .uberX:
+            return "UberX"
+        case .black:
+            return "Uber Black"
+        case .uberXL:
+            return "UberXL"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .uberX:
+            return "uber-x"
+        case .black:
+            return "uber-black"
+        case .uberXL:
+            return "uber-x"
+        }
+    }
+}
